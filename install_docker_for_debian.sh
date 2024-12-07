2c7644160f94f5cb7e52a173e43b704d39b00214
groveer <groveer@outlook.com>
2024-12-07 17:01:55 +0800

    update distcc

--- install_docker_for_debian.sh
+++ install_docker_for_debian.sh
@@ -7,7 +7,7 @@ sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# You can change the distribution to your own Debian version
+# TIP: Change the distribution to your own Debian version
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
@@ -16,3 +16,21 @@ echo \
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# TIP: Change the proxy to your own proxy
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": [
+    "https://hub.geekery.cn",
+    "https://hub.littlediary.cn",
+    "https://docker.rainbond.cc"
+  ],
+  "proxies": {
+    "http-proxy": "http://proxy.groveer.com:7890",
+    "https-proxy": "http://proxy.groveer.com:7890",
+    "no-proxy": "localhost,127.0.0.1,::1"
+  }
+}
+EOF
+
+sudo systemctl enable --now docker
